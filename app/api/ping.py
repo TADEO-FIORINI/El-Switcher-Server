d80985712e90261acb1cc982bbcb642cebbfcad4
Tadeo Salvador Fiorini Niño <tadeo.nino.039@mi.unc.edu.ar>
2025-04-09 23:18:54 -0300

    trying get ping api_route with uptimerobot

--- app/api/ping.py
+++ app/api/ping.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter, status
 
 router = APIRouter(prefix="/ping", tags=["Ping"])
 
-@router.get("")
+@router.api_route("", methods=["GET", "HEAD"])
 def ping():
     return {"status": "ok"}
